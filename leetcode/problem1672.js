fa68fcb488d04959fb7b24c07c1ce8b64fb56139
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-04-23 15:15:53 -0400

    Create problem1672.js

---
+++ leetcode/problem1672.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let totals = []
+  accounts.forEach(account => {
+      let total = account.reduce((a, b) => a + b)
+      totals.push(total)
+  })
+ return Math.max(...totals)
+};
