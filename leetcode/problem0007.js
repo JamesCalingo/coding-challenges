911cbff930c0d64249a04ad09f457a190ae46d3b
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-04-29 14:26:58 -0400

    Create problem0007.js

---
+++ leetcode/problem0007.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let reversed = parseInt(x.toString().split('').reverse().join(''))
+    if(x < 0) {
+        reversed *= -1
+    }
+    if (-(2**31) < reversed && reversed < 2**31 - 1){
+        return reversed}
+    else return 0
+};
+
+// Hoping to optimize the if statement - for some reason, that ended up holding me up for a LONG time (x < y < z didn't work for some reason).
