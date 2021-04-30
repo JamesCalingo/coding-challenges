255fd6b274c2d3002fe8b409fa338a485b9028e1
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-04-30 15:20:49 -0400

    Update problem1688.js

--- leetcode/problem1688.js
+++ leetcode/problem1688.js
@@ -6,12 +6,8 @@ var numberOfMatches = function(n) {
   return n - 1
 };
 
-// I'm going to do something different here and include the actual problem description:
+// https://leetcode.com/problems/count-of-matches-in-tournament/
 
-/**You are given an integer n, the number of teams in a tournament that has strange rules:
-*If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
-*If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
-Return the number of matches played in the tournament until a winner is decided.
-*/
+
 
 // I'm sure there's some fancy algorithm you COULD implement for this, but mathematically, it literally just boils down to n - 1...
