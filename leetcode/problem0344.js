4d8da78dfc1e5fda4e92854a13475b7721993f4b
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-05-28 14:18:45 -0400

    Update and rename problem344.js to problem0344.js

--- leetcode/problem0344.js
+++ leetcode/problem0344.js
@@ -20,6 +20,14 @@ var reverseString = function(s) {
 // BUT! This requires an extra variable, which adds space complexity (imagine creating a second array to reverse "Chargoggagoggmanchauggagoggchaubunagungamaugg").
 // LeetCode wants this done without any extra storage allocations, so to do that:
 
-// (working on this)
+var reverseString = function(s) {
+    for(let i = s.length - 1; i >= 0; i--) {
+            s.push(s[i]);
+    }
+    let half = s.length / 2
+    s.splice(0, half)
+};
+
+// The only issue is that this ends up consistently in the bottom 15% on memory...again, try reversing "Chargoggagoggmanchauggagoggchaubunagungamaugg" with this
 
 // https://leetcode.com/problems/reverse-string/
