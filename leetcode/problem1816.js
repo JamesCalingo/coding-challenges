e4a5b3946c0d97067c047251396c5ed68794dba6
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-06-02 14:43:52 -0400

    Create problem1816.js

---
+++ leetcode/problem1816.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+    let words = s.split(" ");
+    return words.slice(0, k).join(" ");
+};
