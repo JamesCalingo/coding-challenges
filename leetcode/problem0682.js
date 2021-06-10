585a93d8c54ed609d7a306ebc8e68993b7502097
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-06-10 14:47:26 -0400

    Update problem0682.js

--- leetcode/problem0682.js
+++ leetcode/problem0682.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 var calPoints = function(ops) {
-    let score = []
+    let score = [];
     for(let i = 0; i < ops.length; i++){
         if(parseInt(ops[i])){
-            score.push(parseInt(ops[i]))
-        }
+            score.push(parseInt(ops[i]));
+        };
         if(ops[i] === "C"){
-            score.pop()
-        }
+            score.pop();
+        };
         if(ops[i] === "D"){
-            score.push(score[score.length - 1] * 2)
-        }
+            score.push(score[score.length - 1] * 2);
+        };
         if(ops[i] === "+"){
-            score.push(score[score.length - 1] + score[score.length - 2])
-        }
-    }
-return score.reduce((a, b) => a+b)
+            score.push(score[score.length - 1] + score[score.length - 2]);
+        };
+    };
+return score.reduce((a, b) => a+b);
 };
 
 // You're probably thinking that this looks way too verbose - and it is!
@@ -43,9 +43,9 @@ var calPoints = function(ops) {
         
             default:
             score.push(parseInt(ops[i]))
-    }
-    }
-return score.reduce((a, b) => a+b)
+        };
+    };
+return score.reduce((a, b) => a + b);
 };
 
 // It's probably hard to understand for pure beginners, but you can see how it's a bit "easier" than a bunch of if statements.
