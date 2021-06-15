53676af2ff1c537a61e64fd71c235309d609fdfa
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-06-15 11:24:17 -0400

    Create problem0412.js

---
+++ leetcode/problem0412.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let stringArray = []
+    for(let i = 1; i <= n; i++){
+        let indexString = i.toString()
+        
+        if(i % 3 === 0) {
+         indexString = "Fizz"   
+        }
+        if(i % 5 === 0) {
+         indexString = "Buzz"
+        }
+        if(i % 3 === 0 && i % 5 === 0) {
+            indexString = "FizzBuzz"
+        }
+       
+    
+        stringArray.push(indexString)
+    }
+    return stringArray
+};
+
+// That's right: FizzBuzz is on LeetCode!
+
+/** This solution isn't great however. 
+  * As YouTuber Tom Scott puts it, if you need to add a new condition, then attempting to refactor THIS
+  * can lead to more chances for an error to occur.
+  */
+
+// The better way to do this is to concatenate:
+
+var fizzBuzz = function(n) {
+    let stringArray = []
+    for(let i = 1; i <= n; i++){
+        let indexString = ""
+        
+        if(i % 3 === 0) {
+         indexString += "Fizz"   
+        }
+        if(i % 5 === 0) {
+         indexString += "Buzz"
+        }
+        if(indexString = "") {
+          indexString = i.toString()
+    
+        stringArray.push(indexString)
+    }
+    return stringArray
+};
+  
+// https://leetcode.com/problems/fizz-buzz/
+  
+// The Tom Scott video explaining the solution: https://www.youtube.com/watch?v=QPZ0pIK_wsc
