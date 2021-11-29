82267b2955fc4a51a8794c97be59c79ce4d21ac6
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-11-29 14:30:56 -0500

    Update problem0009.js

--- leetcode/problem0009.js
+++ leetcode/problem0009.js
@@ -3,10 +3,10 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let str = x.toString()
-    let splitStr = str.split('')
-    let reverse = splitStr.reverse().join('')
+    const str = x.toString()
+    const reverse = str.split("").reverse().join("")
     return str == reverse
 };
-
 // https://leetcode.com/problems/palindrome-number/
+
+// UPDATE NOV 29: Changing reverse to single line and using const for everything since there's no changes made to variables.
