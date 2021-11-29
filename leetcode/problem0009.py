b8a04cb465ca97be2faf97ad3821579f3d670ede
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2021-11-29 14:28:21 -0500

    Create problem0009.py

---
+++ leetcode/problem0009.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        string_x = str(x)
+        reversed = string_x[::-1]
+        return reversed == string_x
+      
+# This looks a bit neater than the JS solution I feel - it's just that you need to know how to make a reversed string in Python, which is a little unorthodox.
