d092930757ed6ae1b124eac35ad14b64ef1ebb53
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2024-09-24 10:29:44 -0400

    Create problem0001.go

---
+++ leetcode/problem0001.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+    var solution []int
+    for i := 0; i < len(nums); i++ {
+        for j := i + 1; j < len(nums); j++ {
+            if nums[i] + nums[j] == target {
+                solution = append(solution, i, j)
+            }
+        }
+    }
+    return solution
+}
