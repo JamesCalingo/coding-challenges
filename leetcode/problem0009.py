6aad35b24e4e6108288e35c197cfe8488fc109b1
James Calingo <48683010+JamesCalingo@users.noreply.github.com>
2024-09-24 14:34:11 -0400

    Update problem0009.py

--- leetcode/problem0009.py
+++ leetcode/problem0009.py
@@ -5,3 +5,6 @@ class Solution:
         return reversed == string_x
       
 # This looks a bit neater than the JS solution I feel - it's just that you need to know how to make a reversed string in Python, which is a little unorthodox.
+
+# https://leetcode.com/problems/palindrome-number/
+
